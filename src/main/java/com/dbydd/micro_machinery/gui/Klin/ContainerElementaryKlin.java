c5d8d9c80e7182a479bced44a80294762bc682c4
hqyzsy <52950297+hqyzsy@users.noreply.github.com>
2019-11-11 19:26:06 +0800

    change sth in texture（gui and model）

--- src/main/java/com/dbydd/micro_machinery/gui/Klin/ContainerElementaryKlin.java
+++ src/main/java/com/dbydd/micro_machinery/gui/Klin/ContainerElementaryKlin.java
@@ -1,6 +1,8 @@
 package com.dbydd.micro_machinery.gui.Klin;
 
+import com.dbydd.micro_machinery.blocks.tileentities.TileEntityKlin;
 import com.dbydd.micro_machinery.gui.ContainerBase;
+import com.sun.tools.javac.jvm.Items;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.Slot;
 import net.minecraft.item.ItemStack;
@@ -9,7 +11,7 @@ import net.minecraftforge.items.SlotItemHandler;
 
 public class ContainerElementaryKlin extends ContainerBase {
 
-    private ItemStackHandler items = new ItemStackHandler(4);
+    private ItemStackHandler items = new ItemStackHandler(5);
 
     public ContainerElementaryKlin(EntityPlayer player) {
         super();
@@ -18,8 +20,20 @@ public class ContainerElementaryKlin extends ContainerBase {
         this.addSlotToContainer(new SlotItemHandler(items, 1, 40, 50));
         this.addSlotToContainer(new SlotItemHandler(items, 2, 80, 50));
         this.addSlotToContainer(new SlotItemHandler(items, 3, 120, 50));
+        this.addSlotToContainer(new SlotItemHandler(items, 4, 152, 65));
+        for (int i = 0; i < 3; ++i)
+    {
+        for (int j = 0; j < 9; ++j)
+        {
+            this.addSlotToContainer(new Slot(player.inventory, j + i * 9 + 9, 8 + j * 18, 84 + i * 18));
+        }
     }
 
+        for (int i = 0; i < 9; ++i)
+    {
+        this.addSlotToContainer(new Slot(player.inventory, i, 8 + i * 18, 143));
+    }
+};
     @Override
     public ItemStack transferStackInSlot(EntityPlayer playerIn, int index) {
         return null;
