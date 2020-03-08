e031a90736988107ddb8287665d86562971aa66c
hqyzsy <52950297+hqyzsy@users.noreply.github.com>
2020-03-08 19:52:11 +0800

    begin to make generators (add world0 ore) 20200308

--- src/main/java/com/dbydd/micro_machinery/init/ModGenerators.java
+++ src/main/java/com/dbydd/micro_machinery/init/ModGenerators.java
@@ -14,6 +14,23 @@ public class ModGenerators {
     //    public static final List<OreGenRecipe> endOreGenerators = new ArrayList<OreGenRecipe>();
     public static final List<WorldGenerator> worldGenerators = new ArrayList<WorldGenerator>();
 
-    public static final OreGenRecipe test = new OreGenRecipe(ModBlocks.BLOCKBRONZE, new Block[]{Blocks.STONE, Blocks.GRASS, Blocks.DIRT}, 0, 1, 255, 16, 4);
+    public static final OreGenRecipe copper1 = new OreGenRecipe(ModBlocks.ORECOPPER, new Block[]{Blocks.STONE}, 0, 8, 48, 6, 8);
+    //public static final OreGenRecipe copper2 = new OreGenRecipe(ModBlocks.ORECOPPER, new Block[]{Blocks.STONE, 5}, 0, 0, 255, 8, 3);
+    public static final OreGenRecipe tin1 = new OreGenRecipe(ModBlocks.ORETIN, new Block[]{Blocks.STONE}, 0, 16, 56, 8, 4);
+    //public static final OreGenRecipe tin2 = new OreGenRecipe(ModBlocks.ORETIN, new Block[]{Blocks.STONE, 1}, 0, 0, 255, 16, 2);
+    public static final OreGenRecipe silver1 = new OreGenRecipe(ModBlocks.ORESILVER, new Block[]{Blocks.STONE}, 0, 2, 32, 4, 4);
+    //public static final OreGenRecipe silver2 = new OreGenRecipe(ModBlocks.ORESILVER, new Block[]{Blocks.STONE, 3}, 0, 0, 255, 6, 2);
+    public static final OreGenRecipe nickel1 = new OreGenRecipe(ModBlocks.ORENICKEL, new Block[]{Blocks.STONE}, 0, 2, 48, 4, 4);
+    //public static final OreGenRecipe nickel2 = new OreGenRecipe(ModBlocks.ORENICKEL, new Block[]{Blocks.STONE, 5}, 0, 0, 255, 4, 3);
+    public static final OreGenRecipe pyrolusite = new OreGenRecipe(ModBlocks.OREPYROLUSITE, new Block[]{Blocks.SANDSTONE, Blocks.RED_SANDSTONE}, 0, 30, 255, 4, 3);
+    public static final OreGenRecipe graphite = new OreGenRecipe(ModBlocks.OREGRAPHITE, new Block[]{Blocks.COAL_ORE}, 0, 0, 255, 3, 2);
+    public static final OreGenRecipe chromite1 = new OreGenRecipe(ModBlocks.ORECHROMITE, new Block[]{Blocks.STONE}, 0, 4, 20, 4, 4);
+    //public static final OreGenRecipe chromite2 = new OreGenRecipe(ModBlocks.ORECHROMITE, new Block[]{Blocks.STONE, 5}, 0, 0, 255, 6, 2);
+    public static final OreGenRecipe ilmenite1 = new OreGenRecipe(ModBlocks.OREILMENITE, new Block[]{Blocks.STONE}, 0, 0, 16, 2, 3);
+    //public static final OreGenRecipe Ilmenite2 = new OreGenRecipe(ModBlocks.OREILMENITE, new Block[]{Blocks.STONE, 3}, 0, 0, 255, 4, 3);
+    //public static final OreGenRecipe ferromanganese1 = new OreGenRecipe(ModBlocks.OREFERROMANGANESE, new Block[]{Blocks.STONE, 3}, 0, 0, 255, 6, 4);
+    public static final OreGenRecipe ferromanganese2 = new OreGenRecipe(ModBlocks.OREFERROMANGANESE, new Block[]{Blocks.IRON_ORE}, 0, 0, 255, 4, 3);
+    public static final OreGenRecipe bauxite = new OreGenRecipe(ModBlocks.OREBAUXITE, new Block[]{Blocks.GRAVEL, Blocks.DIRT}, 0, 32, 56, 2, 4);
+    public static final OreGenRecipe nolanite = new OreGenRecipe(ModBlocks.ORENOLANITE, new Block[]{Blocks.STONE}, 0, 0, 255, 2, 4);
 
 }
