d7c7b628e29367801e85e4e7db05d17d2fb73643
dbydd <1992003927@qq.com>
2020-03-24 12:53:34 +0800

    generate special vein

--- src/main/java/com/dbydd/micro_machinery/items/special/TestItem.java
+++ src/main/java/com/dbydd/micro_machinery/items/special/TestItem.java
@@ -4,9 +4,11 @@ import com.dbydd.micro_machinery.init.ModItems;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.text.TextComponentString;
+import net.minecraft.world.World;
 
 public class TestItem extends Item {
     public TestItem() {
@@ -16,8 +18,24 @@ public class TestItem extends Item {
     }
 
     @Override
-    public boolean onBlockStartBreak(ItemStack itemstack, BlockPos pos, EntityPlayer player) {
-        player.sendMessage(new TextComponentString(Blocks.STONE.getStateFromMeta(3) == player.getEntityWorld().getBlockState(pos) ? "true" : "false"));
-        return super.onBlockStartBreak(itemstack, pos, player);
+    public EnumActionResult onItemUse(EntityPlayer player, World worldIn, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
+        int veinHeight = 5;
+        int middleY = Math.round(veinHeight / 2);
+        for (int i = 0 - middleY; i <= veinHeight; i++) {
+            int x = pos.getX();
+            int y = pos.getY() + i;
+            int z = pos.getZ();
+            int radius = veinHeight - (Math.abs(i));
+            BlockPos beginPos = new BlockPos(x, y, z);
+            for (int rx = x - radius; rx <= radius + x; rx++) {
+                for (int rz = z - radius; rz <= radius + z; rz++) {
+                    BlockPos position = new BlockPos(rx, y, rz);
+                    if ((Math.pow((x - rx), 2) + Math.pow((z - rz), 2)) <= Math.pow((radius), 2)) {
+                        player.getEntityWorld().setBlockState(position, Blocks.STONE.getDefaultState(), 2);
+                    }
+                }
+            }
+        }
+        return EnumActionResult.SUCCESS;
     }
 }
