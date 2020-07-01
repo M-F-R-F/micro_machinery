4145e181781b4d85b9cdba249b5e0dd51bd0e208
dbydd <1992003927@qq.com>
2020-07-01 14:26:08 +0800

    1.15.2 blocks,hammer,sword,etc.

--- src/main/java/com/dbydd/micro_machinery/registery_lists/RegisteryedBlocks.java
+++ src/main/java/com/dbydd/micro_machinery/registery_lists/RegisteryedBlocks.java
@@ -2,10 +2,28 @@ package com.dbydd.micro_machinery.registery_lists;
 
 import com.dbydd.micro_machinery.blocks.MMBlockBase;
 import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.RenderType;
 
 public class RegisteryedBlocks {
 
-       public static final Block TESTBLOCK =  new MMBlockBase("testblock");
+
+    /**
+     * 空心，即.notSolid()
+     */
+    public static final Block.Properties HOLLOW_PROPERTY = Block.Properties.create(Material.ROCK).notSolid();
+
+    public static final Block EXAMPLE_BLOCK = new MMBlockBase(HOLLOW_PROPERTY, "example_block");
+
+    /**
+     * 可以半透明
+     */
+    public static final Block EXAMPLE_TRANSPARTMENT_BLOCK_1 = new MMBlockBase(HOLLOW_PROPERTY,"example_block_transpartment_1", RenderType.getTranslucent());
+
+    /**
+     * 要么不透明，要么完全透明
+     */
+    public static final Block EXAMPLE_TRANSPARTMENT_BLOCK_2 = new MMBlockBase(HOLLOW_PROPERTY,"example_block_transpartment_1", RenderType.getCutoutMipped());
 
     private RegisteryedBlocks() {
     }
