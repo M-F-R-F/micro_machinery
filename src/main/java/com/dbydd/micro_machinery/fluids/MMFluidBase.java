0c8848368cf3247983db6c140426aa88d89a173c
dbydd <1992003927@qq.com>
2020-07-03 20:26:17 +0800

    1.15.2 fluids

--- src/main/java/com/dbydd/micro_machinery/fluids/MMFluidBase.java
+++ src/main/java/com/dbydd/micro_machinery/fluids/MMFluidBase.java
@@ -3,6 +3,7 @@ package com.dbydd.micro_machinery.fluids;
 import com.dbydd.micro_machinery.Micro_Machinery;
 import com.dbydd.micro_machinery.items.MMItemBase;
 import com.dbydd.micro_machinery.registery_lists.RegisteryedFluids;
+import com.mojang.datafixers.types.Func;
 import net.minecraft.block.Block;
 import net.minecraft.block.FlowingFluidBlock;
 import net.minecraft.fluid.FlowingFluid;
@@ -16,6 +17,7 @@ import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 
 import java.util.function.BiFunction;
+import java.util.function.Function;
 
 public class MMFluidBase {
     private static DeferredRegister<Fluid> FLUID_REGISTER = Micro_Machinery.FLUID_REGISTER;
@@ -29,14 +31,14 @@ public class MMFluidBase {
     public RegistryObject<Item> fluid_bucket;
     public ForgeFlowingFluid.Properties fluid_properties;
 
-    public MMFluidBase(String name, Block.Properties fluid_block_properties, BiFunction<FluidAttributes.Builder, Fluid, FluidAttributes> factory) {
+    public MMFluidBase(String name, Block.Properties fluid_block_properties, Function<FluidAttributes.Builder, FluidAttributes.Builder> factory) {
         this.fluid_resource_location = new ResourceLocation("fluids/" + name + "_still");
         this.fluid_flow_resource_location = new ResourceLocation("fluids/" + name + "_flow");
         this.fluid = FLUID_REGISTER.register(name, () -> new ForgeFlowingFluid.Source(this.fluid_properties));
         this.fluid_flowing = FLUID_REGISTER.register(name + "_flow", () -> new ForgeFlowingFluid.Flowing(this.fluid_properties));
-        this.fluid_block = BLOCK_REGISTER.register(name, () -> new FlowingFluidBlock(this.fluid::get, fluid_block_properties));
-        this.fluid_bucket = ITEM_REGISTER.register(name + "_bucket", () -> new BucketItem(this.fluid::get, MMItemBase.DEFAULT_PROPERTIES));
-        this.fluid_properties = new ForgeFlowingFluid.Properties(this.fluid, this.fluid_flowing, factory.apply(FluidAttributes.builder(fluid_resource_location, fluid_flow_resource_location), this.fluid.get()));
+        this.fluid_block = BLOCK_REGISTER.register(name, () -> new FlowingFluidBlock(this.fluid, fluid_block_properties));
+        this.fluid_bucket = ITEM_REGISTER.register(name + "_bucket", () -> new BucketItem(this.fluid, MMItemBase.DEFAULT_PROPERTIES));
+        this.fluid_properties = new ForgeFlowingFluid.Properties(this.fluid, this.fluid_flowing, factory.apply(FluidAttributes.builder(fluid_resource_location, fluid_flow_resource_location)));
         RegisteryedFluids.registeries.put("name", this);
     }
 }
