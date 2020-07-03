62773364f862f915045e0e60e2f1abbb027d185d
hqyzsy <52950297+hqyzsy@users.noreply.github.com>
2020-07-03 20:47:10 +0800

    20200703 20.47 for fluid

--- src/main/java/com/dbydd/micro_machinery/registery_lists/RegisteryedFluids.java
+++ src/main/java/com/dbydd/micro_machinery/registery_lists/RegisteryedFluids.java
@@ -1,6 +1,8 @@
 package com.dbydd.micro_machinery.registery_lists;
 
 import com.dbydd.micro_machinery.fluids.MMFluidBase;
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -8,6 +10,8 @@ import java.util.Map;
 public class RegisteryedFluids {
     public static Map<String, MMFluidBase> registeries = new HashMap<>();
 
+    public static final MMFluidBase MOLTEN_COPPER = new MMFluidBase("molten_copper", Block.Properties.create(Material.LAVA),(text)->text.luminosity(9).temperature(800).viscosity(5).density(2));
+
     public static void Init() {
-    }
+            }
 }
