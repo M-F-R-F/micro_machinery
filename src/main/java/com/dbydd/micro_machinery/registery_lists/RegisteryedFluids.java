3a5f12ead8322fe08e6138191aa34b3d8a5c28dc
dbydd <1992003927@qq.com>
2020-07-04 09:26:55 +0800

    1.15.2 fluids

--- src/main/java/com/dbydd/micro_machinery/registery_lists/RegisteryedFluids.java
+++ src/main/java/com/dbydd/micro_machinery/registery_lists/RegisteryedFluids.java
@@ -9,7 +9,6 @@ import java.util.Map;
 
 public class RegisteryedFluids {
     public static final MMFluidBase MOLTEN_COPPER = new MMFluidBase("molten_copper", Block.Properties.create(Material.LAVA), (text) -> text.luminosity(9).temperature(800).viscosity(5).density(2));
-    public static Map<String, MMFluidBase> registeries = new HashMap<>();
 
     public static void Init() {
     }
