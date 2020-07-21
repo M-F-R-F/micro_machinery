e446ae2516537521328e122778e30ad5bd900444
dbydd <1992003927@qq.com>
2020-07-21 21:04:25 +0800

    tickrate, etc

--- src/main/java/com/dbydd/micro_machinery/fluids/MMFluidBase.java
+++ src/main/java/com/dbydd/micro_machinery/fluids/MMFluidBase.java
@@ -27,21 +27,26 @@ public class MMFluidBase {
     private static DeferredRegister<Item> ITEM_REGISTER = Micro_Machinery.ITEM_REGISTER;
     public final ResourceLocation fluid_resource_location;
     public final ResourceLocation fluid_flow_resource_location;
+    private final String name;
     public RegistryObject<FlowingFluid> fluid;
     public RegistryObject<FlowingFluid> fluid_flowing;
     public RegistryObject<FlowingFluidBlock> fluid_block;
     public RegistryObject<Item> fluid_bucket;
     public ForgeFlowingFluid.Properties fluid_properties;
-
     public MMFluidBase(String name, Block.Properties fluid_block_properties, Function<FluidAttributes.Builder, FluidAttributes.Builder> factory, int tickRate) {
-        this.fluid_resource_location = new ResourceLocation(Micro_Machinery.NAME,"fluids/" + name + "_still");
-        this.fluid_flow_resource_location = new ResourceLocation(Micro_Machinery.NAME,"fluids/" + name + "_flow");
-        this.fluid = FLUID_REGISTER.register("fluids/"+name, () -> new ForgeFlowingFluid.Source(this.fluid_properties));
-        this.fluid_flowing = FLUID_REGISTER.register("fluids/"+name + "_flow", () -> new ForgeFlowingFluid.Flowing(this.fluid_properties));
-        this.fluid_block = BLOCK_REGISTER.register("fluids/"+name, () -> new FlowingFluidBlock(this.fluid, fluid_block_properties));
+        this.name = name;
+        this.fluid_resource_location = new ResourceLocation(Micro_Machinery.NAME, "fluids/" + name + "_still");
+        this.fluid_flow_resource_location = new ResourceLocation(Micro_Machinery.NAME, "fluids/" + name + "_flow");
+        this.fluid = FLUID_REGISTER.register("fluids/" + name, () -> new ForgeFlowingFluid.Source(this.fluid_properties));
+        this.fluid_flowing = FLUID_REGISTER.register("fluids/" + name + "_flow", () -> new ForgeFlowingFluid.Flowing(this.fluid_properties));
+        this.fluid_block = BLOCK_REGISTER.register("fluids/" + name, () -> new FlowingFluidBlock(this.fluid, fluid_block_properties));
         this.fluid_bucket = ITEM_REGISTER.register(name + "_bucket", () -> new BucketItem(this.fluid, MMItemBase.DEFAULT_PROPERTIES));
         this.fluid_properties = new ForgeFlowingFluid.Properties(this.fluid, this.fluid_flowing, factory.apply(FluidAttributes.builder(fluid_resource_location, fluid_flow_resource_location).density(10).viscosity(1500))).bucket(this.fluid_bucket).block(this.fluid_block).slopeFindDistance(3).explosionResistance(100F).tickRate(tickRate);
         RenderTypes.fluidRenderTypeMap.add(this);
         FluidBucketDispenserRegister.fluids.add(this);
+    }
+
+    public String getName() {
+        return name;
     }
 }
--- src/main/java/com/dbydd/micro_machinery/utils/MoltenMaterialToRockEventDenier.java
+++ src/main/java/com/dbydd/micro_machinery/utils/MoltenMaterialToRockEventDenier.java
@@ -1,9 +1,13 @@
 package com.dbydd.micro_machinery.utils;
 
 import com.dbydd.micro_machinery.Micro_Machinery;
+import com.dbydd.micro_machinery.blocks.MMBlockBase;
 import com.dbydd.micro_machinery.fluids.MMFluidBase;
+import com.sun.org.apache.bcel.internal.generic.FSUB;
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.fluid.Fluid;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
@@ -23,9 +27,16 @@ public class MoltenMaterialToRockEventDenier {
     public static void onMoltenMaterialGenerateStone(BlockEvent.FluidPlaceBlockEvent event) {
         Fluid fluid = event.getWorld().getFluidState(event.getLiquidPos()).getFluid();
         if (fluid.getRegistryName().getNamespace().equals(Micro_Machinery.NAME) && fluid.getTags().contains(new ResourceLocation("minecraft", "lava"))) {
-            //todo
-            event.setNewState(Blocks.DIAMOND_BLOCK.getDefaultState());
+            String path = fluid.getRegistryName().getPath();
+
+            for (MMFluidBase fluidBase : MMFluidBase.fluidBaseList) {
+                if (path.contains(fluidBase.getName())) {
+                    Block block = MMBlockBase.registeries.get(fluidBase.getName()+"_discarded").get();
+                    event.setNewState(block.getDefaultState());
+                }
+            }
         }
-            event.setResult(Event.Result.ALLOW);
+        event.setResult(Event.Result.ALLOW);
     }
+
 }
