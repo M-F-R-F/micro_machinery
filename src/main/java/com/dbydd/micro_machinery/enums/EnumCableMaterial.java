ff444cd6614f4594915008e082fefdc760942e32
hqyzsy <52950297+hqyzsy@users.noreply.github.com>
2020-08-23 22:07:16 +0800

    20200823 22.05 for qihua

--- src/main/java/com/dbydd/micro_machinery/enums/EnumCableMaterial.java
+++ src/main/java/com/dbydd/micro_machinery/enums/EnumCableMaterial.java
@@ -3,11 +3,11 @@ package com.dbydd.micro_machinery.enums;
 import net.minecraft.util.IStringSerializable;
 
 public enum EnumCableMaterial implements IStringSerializable {
-    COPPER(1024, "copper"),
-    NICLEL(4096, "nickel"),
-    ALUMINUM(16384, "aluminum"),
-    TUNGSTEN(65536, "tungsten"),
-    COBALT(262144, "cobalt");
+    COPPER(256, "copper"),
+    NICLEL(1024, "nickel"),
+    ALUMINUM(4096, "aluminum"),
+    TUNGSTEN(16384, "tungsten"),
+    COBALT(65536, "cobalt");
 
 
     private final int transfer;
