3e607bf18ce57106c98e5cfe02c8d7f24009b93c
dbydd <1992003927@qq.com>
2020-08-27 10:28:53 +0800

    50%

--- src/main/java/com/dbydd/micro_machinery/recipes/RecipeHelper.java
+++ src/main/java/com/dbydd/micro_machinery/recipes/RecipeHelper.java
@@ -21,26 +21,38 @@ import java.util.stream.Collectors;
 public class RecipeHelper {
 
     public static KlinItemToFluidRecipe GetKlinItemToFluidRecipe(ItemStack stackInSlot1, ItemStack stackInSlot2, RecipeManager manager) {
-//        List<KlinItemToFluidRecipe> collect = manager.getRecipes().stream().filter(iRecipe -> iRecipe.getType() == RegisteredRecipeSerializers.Type.KLIN_ITEM_TO_FLUID_RECIPE_TYPE).map(iRecipe -> (KlinItemToFluidRecipe) iRecipe).collect(Collectors.toList());
         List<KlinItemToFluidRecipe> collect = getRecipeListByType(manager, RegisteredRecipeSerializers.Type.KLIN_ITEM_TO_FLUID_RECIPE_TYPE);
         for (KlinItemToFluidRecipe recipe : collect) {
             if (recipe.isIssingle()) {
-                if ((recipe.getInput().getItem() == stackInSlot1.getItem()) || (stackInSlot1.getItem() == stackInSlot2.getItem())) {
-                    if (stackInSlot1.getCount() + stackInSlot2.getCount() >= recipe.getInput().getCount()) {
-                        return recipe;
+                if (!(stackInSlot1.isEmpty() && stackInSlot2.isEmpty())) {
+                    if (recipe.isInputTag()) {
+                        if ((stackInSlot1.isEmpty() || stackInSlot1.getItem().getTags().contains(recipe.getTag())) && (stackInSlot2.isEmpty() || stackInSlot2.getItem().getTags().contains(recipe.getTag()))) {
+                            if (stackInSlot1.getCount() + stackInSlot2.getCount() >= recipe.getInput().getCount()) {
+                                return recipe;
+                            }
+                        }
+                    } else {
+                        if ((recipe.getInput().getItem() == stackInSlot1.getItem()) || (stackInSlot1.getItem() == stackInSlot2.getItem())) {
+                            if (stackInSlot1.getCount() + stackInSlot2.getCount() >= recipe.getInput().getCount()) {
+                                return recipe;
+                            }
+                        }
                     }
                 }
-            } else if ((stackInSlot1.getItem() == recipe.getInput1().getItem()) && (stackInSlot2.getItem() == recipe.getInput2().getItem()) || ((stackInSlot2.getItem() == recipe.getInput1().getItem()) && (stackInSlot1.getItem() == recipe.getInput2().getItem()))) {
-                if ((stackInSlot1.getCount() >= recipe.getInput1().getCount()) && (stackInSlot2.getCount() >= recipe.getInput2().getCount()) || ((stackInSlot2.getCount() >= recipe.getInput1().getCount()) && (stackInSlot1.getCount() >= recipe.getInput2().getCount()))) {
-                    return recipe;
-                }
+            } else if (!(stackInSlot1.isEmpty() || stackInSlot2.isEmpty())) {
+                if (((recipe.isInput1Tag() ? stackInSlot1.getItem().getTags().contains(recipe.getTag1()) : (stackInSlot1.getItem() == recipe.getInput1().getItem())) && (recipe.isInput2Tag() ? stackInSlot2.getItem().getTags().contains(recipe.getTag2()) : (stackInSlot2.getItem() == recipe.getInput2().getItem()))) || ((recipe.isInput1Tag() ? stackInSlot2.getItem().getTags().contains(recipe.getTag1()) : (stackInSlot2.getItem() == recipe.getInput1().getItem())) && (recipe.isInput2Tag() ? stackInSlot1.getItem().getTags().contains(recipe.getTag2()) : (stackInSlot1.getItem() == recipe.getInput2().getItem()))))
+
+//                        if ((stackInSlot1.getItem() == recipe.getInput1().getItem()) && (stackInSlot2.getItem() == recipe.getInput2().getItem()) || ((stackInSlot2.getItem() == recipe.getInput1().getItem()) && (stackInSlot1.getItem() == recipe.getInput2().getItem()))) {
+                    if ((stackInSlot1.getCount() >= recipe.getInput1().getCount()) && (stackInSlot2.getCount() >= recipe.getInput2().getCount()) || ((stackInSlot2.getCount() >= recipe.getInput1().getCount()) && (stackInSlot1.getCount() >= recipe.getInput2().getCount()))) {
+                        return recipe;
+                    }
+//                        }
             }
         }
         return null;
     }
 
     public static KlinFluidToItemRecipe GetKlinFluidRecipe(FluidStack fluidStack, ItemStack castslot, RecipeManager manager) {
-//        List<KlinFluidToItemRecipe> collect = manager.getRecipes().stream().filter(iRecipe -> iRecipe.getType() == RegisteredRecipeSerializers.Type.KLIN_FLUID_TP_ITEM_RECIPE_TYPE).map(iRecipe -> (KlinFluidToItemRecipe) iRecipe).collect(Collectors.toList());
         List<KlinFluidToItemRecipe> collect = getRecipeListByType(manager, RegisteredRecipeSerializers.Type.KLIN_FLUID_TP_ITEM_RECIPE_TYPE);
         Item cast = castslot.getItem();
         if (cast instanceof MMCastBase) {
@@ -81,7 +93,7 @@ public class RecipeHelper {
         return ForgeRegistries.FLUIDS.getValue(new ResourceLocation(name));
     }
 
-    public static <cast> List<cast> getRecipeListByType(RecipeManager manager, IRecipeType<?> type){
+    public static <cast> List<cast> getRecipeListByType(RecipeManager manager, IRecipeType<?> type) {
         return manager.getRecipes().stream().filter(iRecipe -> iRecipe.getType() == type).map(iRecipe -> (cast) iRecipe).collect(Collectors.toList());
     }
 
--- src/main/java/com/dbydd/micro_machinery/recipes/klin/KlinItemToFluidRecipe.java
+++ src/main/java/com/dbydd/micro_machinery/recipes/klin/KlinItemToFluidRecipe.java
@@ -17,8 +17,6 @@ import net.minecraftforge.items.wrapper.RecipeWrapper;
 import net.minecraftforge.registries.ForgeRegistryEntry;
 
 import javax.annotation.Nullable;
-import java.util.ArrayList;
-import java.util.List;
 
 public class KlinItemToFluidRecipe implements IRecipe<RecipeWrapper> {
 
@@ -29,7 +27,15 @@ public class KlinItemToFluidRecipe implements IRecipe<RecipeWrapper> {
     private final ItemStack input;
     private final FluidStack outputfluidstack;
     private final ResourceLocation id;
-
+    private final boolean isInputTag;
+    private final ResourceLocation tag;
+    private final int count;
+    private final boolean isInput1Tag;
+    private final ResourceLocation tag1;
+    private final int count1;
+    private final boolean isInput2Tag;
+    private final ResourceLocation tag2;
+    private final int count2;
 
     public KlinItemToFluidRecipe(FluidStack outputfluidstack, ItemStack input1, ItemStack input2, int melttime, ResourceLocation id) {
         this.input1 = input1;
@@ -39,6 +45,15 @@ public class KlinItemToFluidRecipe implements IRecipe<RecipeWrapper> {
         this.outputfluidstack = outputfluidstack;
         this.melttime = melttime;
         this.issingle = false;
+        this.isInput1Tag = false;
+        this.isInput2Tag = false;
+        this.tag1 = null;
+        this.tag2 = null;
+        this.isInputTag = false;
+        this.tag = null;
+        this.count = 0;
+        this.count1 = 0;
+        this.count2 = 0;
 
     }
 
@@ -50,7 +65,72 @@ public class KlinItemToFluidRecipe implements IRecipe<RecipeWrapper> {
         this.outputfluidstack = outputfluidstack;
         this.melttime = melttime;
         this.issingle = true;
+        this.isInput1Tag = false;
+        this.isInput2Tag = false;
+        this.tag1 = null;
+        this.tag2 = null;
+        this.isInputTag = false;
+        this.tag = null;
+        this.count = 0;
+        this.count1 = 0;
+        this.count2 = 0;
+    }
 
+    public KlinItemToFluidRecipe(int melttime, FluidStack outputfluidstack, ResourceLocation id, ResourceLocation tag1, ResourceLocation tag2, int count1, int count2) {
+        this.issingle = true;
+        this.melttime = melttime;
+        this.outputfluidstack = outputfluidstack;
+        this.id = id;
+        this.input = ItemStack.EMPTY;
+        this.input1 = ItemStack.EMPTY;
+        this.input2 = ItemStack.EMPTY;
+        this.tag1 = tag1;
+        this.tag2 = tag2;
+        this.isInput1Tag = true;
+        this.isInput2Tag = true;
+        this.tag = null;
+        this.isInputTag = false;
+        this.count = 0;
+        this.count1 = count1;
+        this.count2 = count2;
+    }
+
+    public KlinItemToFluidRecipe(int melttime, FluidStack outputfluidstack, ResourceLocation id, String tag, int count) {
+        this.issingle = false;
+        this.melttime = melttime;
+        this.outputfluidstack = outputfluidstack;
+        this.id = id;
+        this.input = ItemStack.EMPTY;
+        this.input1 = ItemStack.EMPTY;
+        this.input2 = ItemStack.EMPTY;
+        this.tag1 = null;
+        this.tag2 = null;
+        this.isInput1Tag = true;
+        this.isInput2Tag = true;
+        this.tag = null;
+        this.isInputTag = false;
+        this.count = count;
+        this.count2 = 0;
+        this.count1 = 0;
+    }
+
+    public KlinItemToFluidRecipe(int melttime, ItemStack input1, ItemStack input2, ItemStack input, FluidStack outputfluidstack, ResourceLocation id, boolean isInputTag, ResourceLocation tag, boolean isInput1Tag, ResourceLocation tag1, boolean isInput2Tag, ResourceLocation tag2, int count, int count1, int count2) {
+        this.issingle = false;
+        this.melttime = melttime;
+        this.input1 = input1;
+        this.input2 = input2;
+        this.input = input;
+        this.outputfluidstack = outputfluidstack;
+        this.id = id;
+        this.isInputTag = isInputTag;
+        this.tag = tag;
+        this.isInput1Tag = isInput1Tag;
+        this.tag1 = tag1;
+        this.isInput2Tag = isInput2Tag;
+        this.tag2 = tag2;
+        this.count1 = count1;
+        this.count2 = count2;
+        this.count = count;
     }
 
     public ItemStack getInput1() {
@@ -112,6 +192,42 @@ public class KlinItemToFluidRecipe implements IRecipe<RecipeWrapper> {
         return RegisteredRecipeSerializers.Type.KLIN_ITEM_TO_FLUID_RECIPE_TYPE;
     }
 
+    public boolean isInputTag() {
+        return isInputTag;
+    }
+
+    public ResourceLocation getTag() {
+        return tag;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public boolean isInput1Tag() {
+        return isInput1Tag;
+    }
+
+    public ResourceLocation getTag1() {
+        return tag1;
+    }
+
+    public int getCount1() {
+        return count1;
+    }
+
+    public boolean isInput2Tag() {
+        return isInput2Tag;
+    }
+
+    public ResourceLocation getTag2() {
+        return tag2;
+    }
+
+    public int getCount2() {
+        return count2;
+    }
+
     @Override
     public String toString() {
         return "KlinItemToFluidRecipe{" +
@@ -133,17 +249,44 @@ public class KlinItemToFluidRecipe implements IRecipe<RecipeWrapper> {
             FluidStack result = new FluidStack(RecipeHelper.getFluidByName(JSONUtils.getString(output, "fluidName")), JSONUtils.getInt(output, "amount"));
             if (isSingle) {
                 JsonObject inputIfSingle = JSONUtils.getJsonObject(json, "inputIfSingle");
-                Item item = JSONUtils.getItem(inputIfSingle, "item");
                 int count = JSONUtils.getInt(inputIfSingle, "count");
+                boolean isTag = JSONUtils.getBoolean(inputIfSingle, "isTag");
+
+                if (isTag) {
+                    String tag = JSONUtils.getString(inputIfSingle, "tag");
+                    return new KlinItemToFluidRecipe(meltTime, result, recipeId, tag, count);
+                }
+
+                Item item = JSONUtils.getItem(inputIfSingle, "item");
                 return new KlinItemToFluidRecipe(result, new ItemStack(item, count), meltTime, recipeId);
+
             } else {
+
                 JsonObject input = JSONUtils.getJsonObject(json, "input");
                 JsonObject input1 = JSONUtils.getJsonObject(input, "input1");
-                Item item1 = JSONUtils.getItem(input1, "item");
                 int count1 = JSONUtils.getInt(input1, "count");
                 JsonObject input2 = JSONUtils.getJsonObject(input, "input2");
-                Item item2 = JSONUtils.getItem(input2, "item");
                 int count2 = JSONUtils.getInt(input2, "count");
+                boolean isInput2Tag = JSONUtils.getBoolean(input2, "isTag");
+                boolean isInput1Tag = JSONUtils.getBoolean(input1, "isTag");
+
+                if (isInput1Tag) {
+                    String input1Tag = JSONUtils.getString(input1, "tag");
+                    if (isInput2Tag) {
+                        String input2Tag = JSONUtils.getString(input2, "tag");
+                        return new KlinItemToFluidRecipe(meltTime, ItemStack.EMPTY, ItemStack.EMPTY, ItemStack.EMPTY, result, recipeId, false, null, isInput1Tag, new ResourceLocation(input1Tag), isInput2Tag, new ResourceLocation(input2Tag), 0, count1, count2);
+                    }
+                    Item item2 = JSONUtils.getItem(input2, "item");
+                    return new KlinItemToFluidRecipe(meltTime, ItemStack.EMPTY, new ItemStack(item2, count2), ItemStack.EMPTY, result, recipeId, false, null, isInput1Tag, new ResourceLocation(input1Tag), isInput2Tag, null, 0, count1, 0);
+                }
+
+                Item item1 = JSONUtils.getItem(input1, "item");
+                if (isInput2Tag) {
+                    String input2Tag = JSONUtils.getString(input2, "tag");
+                    return new KlinItemToFluidRecipe(meltTime, new ItemStack(item1, count1), ItemStack.EMPTY, ItemStack.EMPTY, result, recipeId, false, null, false, null, isInput2Tag, new ResourceLocation(input2Tag), 0, 0, count2);
+                }
+
+                Item item2 = JSONUtils.getItem(input2, "item");
                 return new KlinItemToFluidRecipe(result, new ItemStack(item2, count2), meltTime, recipeId);
             }
         }
