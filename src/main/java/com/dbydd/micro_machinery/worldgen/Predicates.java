41be9454282cb788499db378d70045ec064ba176
dbydd <1992003927@qq.com>
2020-08-30 11:56:34 +0800

    50%

--- src/main/java/com/dbydd/micro_machinery/worldgen/Predicates.java
+++ src/main/java/com/dbydd/micro_machinery/worldgen/Predicates.java
@@ -3,6 +3,8 @@ package com.dbydd.micro_machinery.worldgen;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.material.Material;
+import net.minecraft.world.World;
+import net.minecraft.world.biome.Biome;
 import net.minecraftforge.registries.ForgeRegistries;
 
 import java.util.Arrays;
@@ -12,7 +14,7 @@ import java.util.stream.Collectors;
 
 public enum Predicates implements Predicate<BlockState> {
     STONE(ForgeRegistries.BLOCKS.getValues().stream().flatMap(block -> block.getStateContainer().getValidStates().stream()).filter(blockState -> blockState.getMaterial() == Material.ROCK).collect(Collectors.toList())),
-    NETHER(Arrays.asList(Blocks.MAGMA_BLOCK.getDefaultState(), Blocks.NETHERRACK.getDefaultState(), Blocks.GRAVEL.getDefaultState(), Blocks.SOUL_SAND.getDefaultState())),
+    NETHER(Arrays.asList(Blocks.MAGMA_BLOCK.getDefaultState(), Blocks.NETHERRACK.getDefaultState(), Blocks.SOUL_SAND.getDefaultState())),
     END(Arrays.asList(Blocks.END_STONE.getDefaultState()));
 
     private final Collection<BlockState> blocks;
@@ -25,4 +27,5 @@ public enum Predicates implements Predicate<BlockState> {
     public boolean test(BlockState block) {
         return blocks.contains(block);
     }
+
 }
