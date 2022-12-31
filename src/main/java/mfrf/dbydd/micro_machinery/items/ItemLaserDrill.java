235e331daef70cebb21a74747b5c872a997464fc
dbydd <1992003927@qq.com>
2022-12-31 12:01:38 +0800

    damn stupid fmlat

--- src/main/java/mfrf/dbydd/micro_machinery/items/ItemLaserDrill.java
+++ src/main/java/mfrf/dbydd/micro_machinery/items/ItemLaserDrill.java
@@ -5,20 +5,25 @@ import mfrf.dbydd.micro_machinery.Micro_Machinery;
 import mfrf.dbydd.micro_machinery.utils.EnergyItemHandler;
 import mfrf.dbydd.micro_machinery.utils.FEContainer;
 import net.minecraft.block.BlockState;
-import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUseContext;
 import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.particles.ParticleTypes;
 import net.minecraft.util.ActionResultType;
+import net.minecraft.util.DamageSource;
 import net.minecraft.util.Hand;
+import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
-import net.minecraft.world.World;
+import net.minecraft.world.server.ServerWorld;
 import net.minecraftforge.common.capabilities.ICapabilityProvider;
+import net.minecraftforge.energy.CapabilityEnergy;
 
 import javax.annotation.Nullable;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class ItemLaserDrill extends MMItemBase {
 
@@ -54,78 +59,57 @@ public class ItemLaserDrill extends MMItemBase {
     public ActionResultType onItemUse(ItemUseContext context) {
         Hand hand = context.getHand();
         PlayerEntity player = context.getPlayer();
-        World world = context.getWorld();
-        BlockPos pos = context.getPos();
-        BlockPos playerPos = player.getPosition();
-        double dist = Math.sqrt(playerPos.distanceSq(pos));
+        ServerWorld world = ((ServerWorld) context.getWorld());
+        Vec3d lookVec = player.getLookVec();
+        Vec3d positionVec = player.getPositionVec();
         ItemStack heldItem = player.getHeldItem(hand);
-        Vec3d hitVec = context.getHitVec();
 
         if (!world.isRemote()) {
+
             if (heldItem.getItem() instanceof ItemLaserDrill) {
-//            heldItem.getCapability(CapabilityEnergy.ENERGY).ifPresent(iEnergyStorage -> {
-//                Optional<Pair<Entity, Double>> min = world.getEntitiesWithinAABB(Entity.class, new AxisAlignedBB(pos, playerPos))
-//                        .stream()
-//                        .map(entity -> new Pair<Entity, Double>(entity, lookatEntity(player, entity, dist)))
-//                        .filter(pair -> pair.getValue() != -1)
-//                        .min(Comparator.comparingDouble(Pair::getValue));
-//                min.ifPresent(entityDoublePair -> {
-//                    Entity key = entityDoublePair.getKey();
-//                    if (key instanceof LivingEntity) {
-//                        try {
-//                            Method damageEntity = LivingEntity.class.getMethod("damageEntity", DamageSource.class, float.class);
-//                            damageEntity.invoke(key, DamageSource.causePlayerDamage(player), Config.LASER_DRILL_DAMAGE.get());
-//                        } catch (NoSuchMethodException e) {
-//                            e.printStackTrace();
-//                        } catch (InvocationTargetException e) {
-//                            e.printStackTrace();
-//                        } catch (IllegalAccessException e) {
-//                            e.printStackTrace();
-//                        }
-//                    }
-//                });
-//
-//            });
 
-                BlockState blockState = world.getBlockState(pos);
+                Vec3d start = positionVec.add(lookVec.scale(0.3));
 
+                AtomicReference<BlockPos> blockPos = null;
+                heldItem.getCapability(CapabilityEnergy.ENERGY).ifPresent(iEnergyStorage -> {
+                    int extracted = iEnergyStorage.extractEnergy(100, false);
+                    if (extracted == 100) {
+                        if (world.getDayTime() % 10 == 0) {
+                            for (int i = 0; i < 20000; i++) {
+                                //step = 0.8
+                                Vec3d add = positionVec.add(lookVec.scale(1 + 0.8 * i));
+                                blockPos.set(new BlockPos(add));
+                                BlockState blockState = world.getBlockState(blockPos.get());
+                                if (!blockState.isAir()) {
+                                    world.destroyBlock(blockPos.get(), true, player);
+                                    break;
+                                } else {
+                                    blockPos.set(null);
+                                }
+                                List<LivingEntity> loadedEntitiesWithinAABB = world.getLoadedEntitiesWithinAABB(LivingEntity.class, new AxisAlignedBB(add.add(-0.3, -0.3, -0.3), add.add(0.3, 0.3, 0.3)));
+                                if (!loadedEntitiesWithinAABB.isEmpty()) {
+                                    for (LivingEntity livingEntity : loadedEntitiesWithinAABB) {
+                                        // stupid
+//                                        ((DamageEntity) livingEntity).DamageEntity(DamageSource.MAGIC, 4);
+                                        livingEntity.damageEntity(DamageSource.IN_FIRE, 4);
+                                    }
+                                    break;
+                                }
+                            }
+                        }
 
-//todo complete mine
-                for (double d = 0; d < dist; d += 0.2) {
-                    Vec3d playerPositionVec = player.getPositionVec();
-                    Vec3d partPos = playerPositionVec.add(hitVec.scale(d));
-                    Vec3d speed = hitVec.scale(0.1);
-                    world.addParticle(ParticleTypes.FLAME, partPos.x, partPos.y, partPos.z, speed.x, speed.y, speed.z);
-                    double[] randPosOffs = random.doubles(30, 0.1, 0.3).toArray();
-                    double[] randSpeedOffsets = random.doubles(30, 0, 0.5).toArray();
-                    for (int i = 0; i < 10; i++) {
-                        world.addParticle(
-                                ParticleTypes.FLAME,
-                                partPos.x + randPosOffs[random.nextInt(30)],
-                                partPos.y + 2 + randPosOffs[random.nextInt(30)],
-                                partPos.z + randPosOffs[random.nextInt(30)],
-//                            speed.x + randSpeedOffsets[random.nextInt(30)],
-//                            speed.y + randSpeedOffsets[random.nextInt(30)],
-//                            speed.z + randSpeedOffsets[random.nextInt(30)]
-                                0, 0.5, 0
-                                //todo check particle
-                        );
                     }
+                });
+                if (blockPos.get() == null) {
+                    Vec3d end = positionVec.add(lookVec.scale(80));
+                    world.spawnParticle(ParticleTypes.FLAME, start.x, start.y, start.z, 24, end.x, end.y, end.z, 2.5);
                 }
             }
-        } else {
         }
         return ActionResultType.SUCCESS;
     }
 
 
-    private double lookatEntity(PlayerEntity player, Entity entity, Double max) {
-        Vec3d vec3d = player.getLook(1.0F).normalize();
-        Vec3d vec3d1 = new Vec3d(entity.getPosX() - player.getPosX(), entity.getPosYEye() - player.getPosYEye(), entity.getPosZ() - player.getPosZ());
-        double d0 = vec3d1.length();
-        vec3d1 = vec3d1.normalize();
-        double d1 = vec3d.dotProduct(vec3d1);
-        double length = vec3d.length();
-        return d1 > 1.0D - 0.025D / d0 && player.canEntityBeSeen(entity) || length <= max ? vec3d1.length() : -1.0;
-    }
+//    private  nearEntity(Vec3d sampleP, World world, Double range) {
+//    }
 }
