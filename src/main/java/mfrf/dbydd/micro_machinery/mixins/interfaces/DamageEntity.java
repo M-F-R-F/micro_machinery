6e038c1fd99d5bf02ef115e13e494764551f05ff
dbydd <1992003927@qq.com>
2023-01-08 15:11:27 +0800

    changed

--- src/main/java/mfrf/dbydd/micro_machinery/mixins/interfaces/DamageEntity.java
+++
@@ -1,7 +0,0 @@
-package mfrf.dbydd.micro_machinery.mixins.interfaces;
-
-import net.minecraft.util.DamageSource;
-
-public interface DamageEntity {
-    void DamageEntity(DamageSource damageSrc, float damageAmount);
-}
--- 参考/src/main/java/com/dbydd/micro_machinery/exceptions/ErrorNumberException.java
+++
@@ -1,7 +0,0 @@
-package com.dbydd.micro_machinery.exceptions;
-
-public class ErrorNumberException extends Exception {
-    public ErrorNumberException(String message) {
-        super(message);
-    }
-}
