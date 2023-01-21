bd3c229500079a9cda8146fe1c6c0e48187db684
hqyzsy <1464036796@qq.com>
2023-01-21 14:05:30 +0800

    杂项 2023.1.21

---
+++ src/main/resources/assets/micro_machinery/models/item/_make.bat
@@ -0,0 +1,25 @@
+@echo off
+:: Vazkii's JSON creator for blocks
+:: Put in your /resources/assets/%modid%/models/block
+:: Makes basic block JSON files as well as the acossiated item and simple blockstate
+:: Can make multiple blocks at once
+::
+:: Usage:
+:: _make (block name 1) (block name 2) (block name x)
+::
+:: Change this to your mod's ID
+set modid=micro_machinery
+
+setlocal enabledelayedexpansion
+
+for %%x in (%*) do (
+	echo Making %%x.json block
+	(
+		echo {
+		echo 	"parent": "item/generated",
+		echo 	"textures": {
+		echo 		"layer0": "%modid%:items/%%x"
+		echo 	}
+		echo }
+	) > %%x.json
+)
