f87850d4bdae2e9b877ed3600ddb1cf899c6ee08
dbydd <1992003927@qq.com>
2023-07-01 17:02:36 +0800

    initialize

--- src/main/java/mfrf/micro_machinery/datagen/DataGenHandler.java
+++ src/main/java/mfrf/micro_machinery/datagen/DataGenHandler.java
@@ -1,17 +1,27 @@
 package mfrf.micro_machinery.datagen;
 
+import mfrf.micro_machinery.MicroMachinery;
+import net.minecraft.core.HolderLookup;
 import net.minecraft.data.DataGenerator;
+import net.minecraft.data.PackOutput;
 import net.minecraft.world.level.levelgen.feature.Feature;
 import net.minecraftforge.data.event.GatherDataEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.RegistryBuilder;
 
+import java.util.concurrent.CompletableFuture;
+
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
 public class DataGenHandler {
 
     @SubscribeEvent
-    public static void gatherData(GatherDataEvent event){
+    public static void gatherData(GatherDataEvent event) {
         DataGenerator generator = event.getGenerator();
+        PackOutput packOutput = generator.getPackOutput();
+        CompletableFuture<HolderLookup.Provider> lookupProvider = event.getLookupProvider();
+        BlockPropertys blockPropertys = new BlockPropertys(packOutput, lookupProvider, MicroMachinery.MODID, event.getExistingFileHelper());
+
+        generator.addProvider(event.includeServer(), blockPropertys);
     }
 }
